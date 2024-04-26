4c6eb2188c919c5720b730a0b0c897d24a993137
renzo-guerra <renzoguerralatt@gmail.com>
2024-04-26 16:37:33 -0300

    DELETE: Unused js file

--- cards-04/card.js
+++
@@ -1,8 +0,0 @@
-let buttons = document.querySelectorAll(".card__specs button").forEach(button => {
-  button.addEventListener("click", (e) => {
-    e.preventDefault();
-    button.classList.toggle("liked");
-    button.children.item(0).classList.toggle('fa-solid');
-    button.children.item(0).classList.toggle('fa-regular');
-  });
-})
